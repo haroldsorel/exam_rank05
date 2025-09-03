409fe9f3475c93157ab3ab755afa9a7fc5a290c9
Harold Sorel <hsorel@mi-r2-p1.s19.be>
2025-09-03 14:46:08 +0200

    life

--- lvl2/life/life.c
+++ lvl2/life/life.c
@@ -6,7 +6,7 @@
 /*   By: haroldsorel <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 23:51:29 by haroldsorel       #+#    #+#             */
-/*   Updated: 2025/09/03 02:03:40 by haroldsorel      ###   ########.fr       */
+/*   Updated: 2025/09/03 14:28:19 by hsorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	print_board(t_board board)
 	while (col < board.height)
 	{
 		line = 0;
-		while (line < board.height)
+		while (line < board.width)
 		{
 			putchar(board.board[col][line]);
 			line++;
@@ -28,6 +28,48 @@ void	print_board(t_board board)
 		col++;
 		putchar('\n');
 	}
+}
+
+void	free_copy(char **copy, t_board *board);
+
+char**	copy_board(t_board *board)
+{
+	int	col = 0;
+	int	line = 0;
+
+	char **copy = malloc(board->height * sizeof(char *));
+	if (copy == NULL)
+		return (NULL);
+	while (col < board->height)
+	{
+		line = 0;
+		copy[col] = malloc(board->width * (sizeof(char)));
+		if (copy[col] == NULL)
+		{
+			free_copy(copy, board);
+			return (NULL);
+		}
+		while (line < board->width)
+		{
+			copy[col][line] = board->board[col][line];
+			line++;
+		}
+		col++;
+	}
+	return (copy);
+}
+
+void	free_copy(char **copy, t_board *board)
+{
+	int	col = 0;
+
+	while (col < board->height)
+	{
+		if (copy[col] != NULL)
+			free(copy[col]);
+		col++;
+	}
+	free(copy);
 }
 
 void	free_board(t_board *board)
@@ -38,9 +80,8 @@ void	free_board(t_board *board)
 		return ;
 	while (col < board->height)
 	{
-		if (board->board[col] == NULL)
-			return ;
-		free(board->board[col]);
+		if (board->board[col] != NULL)
+			free(board->board[col]);
 		col++;
 	}
 	free(board->board);
@@ -60,7 +101,7 @@ int	init_board(t_board *board, char **argv)
 	while (col < board->height)
 	{
 		line = 0;
-		board->board[col] = malloc(board->width * (sizeof(char) + 1));
+		board->board[col] = malloc(board->width * (sizeof(char)));
 		if (board->board[col] == NULL)
 		{
 			free_board(board);
@@ -68,7 +109,7 @@ int	init_board(t_board *board, char **argv)
 		}
 		while (line < board->width)
 		{
-			board->board[col][line] = '0';
+			board->board[col][line] = ' ';
 			line++;
 		}
 		col++;
@@ -88,10 +129,6 @@ int	draw_life(t_board *board)
 	// REMEMBER READ(FD, BUFFER, SIZE) returns 1 on success, 0 on EOF and -1 on error
 	while ((ret = read(0, &buffer, 1)) == 1)
 	{
-		putchar(buffer);
-		putchar(col + '0');
-		putchar(line + '0');
-		putchar('\n');
 		if (buffer == 'w')
 		{
 			if (col == 0)
@@ -146,7 +183,7 @@ int	draw_life(t_board *board)
 		//	break;
 		//}
 		if (pen_down == 1)
-			board->board[col][line] = 'X';
+			board->board[col][line] = '0';
 	}
 	if (ret == -1)
 	{
@@ -156,37 +193,82 @@ int	draw_life(t_board *board)
 	return (1);
 }
 
-//damn this is long
-void	alive_life(t_board *board)
+void	alive_life(t_board *board, char **copy)
 {
-	int col = 0;
-	int line = 0;
+	int	col = 0;
+	int	line = 0;
+	int	friends = 0;
+	int	alive = 0;
 
+	//Here you have to make a copy of the board so you keep the initial state
 	while (col < board->height)
 	{
 		line = 0;
 		while (line < board->width)
 		{
+			alive = 0;
+			friends = 0;
+			if (copy[col][line] == '0')
+				alive = 1;
+
+			//border checking and friends incrementing
+			if (col != 0 && copy[col - 1][line] == '0')
+				friends++;
+			if (col != 0 && line != 0 && copy[col - 1][line - 1] == '0')
+				friends++;
+			if (col != 0 && line != board->width - 1 && copy[col - 1][line + 1] == '0')
+				friends++;
+			if (col != board->height - 1 && copy[col + 1][line] == '0')
+				friends++;
+			if (col != board->height - 1 && line != 0 && copy[col + 1][line - 1] == '0')
+				friends++;
+			if (col != board->height - 1 && line != board->width - 1 && copy[col + 1][line + 1] == '0')
+				friends++;
+			if (line != 0 && copy[col][line - 1] == '0')
+				friends++;
+			if (line != board->width - 1 && copy[col][line + 1] == '0')
+				friends++;
+
+			//checking if the element has to be alived
 			
-			line++;		
+			if (alive == 1 && friends < 2)
+				board->board[col][line]	= ' ';
+			else if (alive == 1 && (friends == 2 || friends == 3))
+				board->board[col][line]	= '0';
+			else if (alive == 1 && friends > 3)
+				board->board[col][line]	= ' ';
+			else if (alive == 0 && friends == 3)
+				board->board[col][line]	= '0';
+			line++;
 		}
+		col++;
 	}
 }
 
-void	loop_life(t_board *board, int iteration)
+int	loop_life(t_board *board, int iteration)
 {
 	int i = 0;
-	int col;
-	int line;
+	char **copy;
 
 	while (i < iteration)
 	{
-		alive_life(board);
+		copy = copy_board(board);
+		if (copy == NULL)
+		{
+			free_board(board);
+			return (-1);
+		}
+		alive_life(board, copy);
 		i++;
+		print_board(*board);
+		sleep(1);
+		free_copy(copy, board);
+		putchar('\n');
 	}
+	return (1);
 }
 
-int	isnumber(char **str)
+int	isnumber(char *str)
 {
 	int i = 0;
 
@@ -207,29 +289,56 @@ int	input(char **argv)
 
 	while (argv[i])
 	{
-		if (isnumber(argv[i][0]))
+		if (isnumber(argv[i]) == 0)
 			return (-1);
 		i++;
 	}
-	if (atoi(argv[1]) == 0 || atoi(arg[2]) == 0)
+	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
 		return (-1);
 	return (1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i = 0;
+	
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
 }
 
 int main(int argc, char **argv)
 {
 	t_board	board;
 
-	if (argc != 4 || input(argv) == -1)
+	if (argc != 4 ||input(argv) == -1)
+	{
+		ft_putstr("Wrong Input");
 		return (1);
+	}
 	argv++;
 	if (init_board(&board, argv) == -1)
+	{
+		ft_putstr("Init Failure");
 		return (1);
+	}
 	if (draw_life(&board) == -1)
+	{
+		ft_putstr("Draw Failure");
 		return (1);
+	}
 	print_board(board);
-	loop_life(&board, atoi(argv[3]));
+	sleep(1);
+	putchar('\n');
+	if (loop_life(&board, atoi(argv[2])) == -1)
+	{
+		ft_putstr("Loop Failure");
+		return (1);
+	}
 	free_board(&board);
+	return (0);
 }
 
 
